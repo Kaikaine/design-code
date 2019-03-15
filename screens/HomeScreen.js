606b107cee8525f6602c0b3028c77e906da18c5f
kaikaine <kairijb@protonmail.com>
2019-03-15 15:05:59 -0500

    props are passed to section screen now

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -129,7 +129,9 @@ class HomeScreen extends React.Component {
                     <TouchableOpacity
                       key={i}
                       onPress={() => {
-                        this.props.navigation.push("Section");
+                        this.props.navigation.push("Section", {
+                          section: card
+                        });
                       }}
                     >
                       <Card
